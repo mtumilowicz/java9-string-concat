78e5492cef8e2ccdcbddd12b5c0c839504d56e21
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-14 22:59:01 +0100

    tests

---
+++ src/test/java/StringConcatBenchmarkTest.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+
+/**
+ * Created by mtumilowicz on 2018-11-14.
+ */
+public class StringConcatBenchmarkTest {
+    
+    @Test
+    public void nonLoopConcatenation() {
+        String a = "a";
+        String b = "b";
+        System.out.println(a + b);
+    }
+
+    @Test
+    public void nonLoopConcatenation2() {
+        System.out.println(new StringBuilder().append("a").append("b"));
+    }
+    
+    @Test
+    public void test() {
+        long start = System.currentTimeMillis();
+        
+        String result = "";
+        
+        for (int i = 0; i < 50_000; i++) {
+            result += i;
+        }
+
+        System.out.println(result);
+
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    @Test
+    public void test2() {
+        long start = System.currentTimeMillis();
+
+        StringBuilder result = new StringBuilder(50_000);
+
+        for (int i = 0; i < 50_000; i++) {
+            result.append(i);
+        }
+
+        System.out.println(result);
+
+        System.out.println(System.currentTimeMillis() - start);
+    }
+}
